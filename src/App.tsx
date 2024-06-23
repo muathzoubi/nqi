ad5ec9f754d53ff8b5e88a462f0a1d0914310865
muathzoubi <32450805+muathzoubi@users.noreply.github.com>
2024-06-23 08:10:09 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img
+            src={
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQglYENp1HMExjOU0WD_GZPWPlArj64Vx19sw&s'
+            }
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+      </div>
+      <h1>موقع مياة نقي</h1>
+      <div className="card">
+        <button>
+          <a href="#">الذهاب للموقع</a>
+        </button>
+        <p>حلي مويتك مع نقي</p>
+      </div>
+      <p className="read-the-docs">جميع الحقوق محفوظة </p>
+    </>
+  );
+}
+
+export default App;
