e69f8e2c3a075432ca6663fb7b2ba3d786bff4be
muathzoubi <moathaza@gmail.com>
2024-06-26 01:54:35 +0300

    zxvx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img
-            src={
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQglYENp1HMExjOU0WD_GZPWPlArj64Vx19sw&s'
-            }
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-      </div>
-      <h1>موقع مياة نقي</h1>
+        
+      </div> 
+      <h1> مياة نقي</h1>
+
+
+
+
+
+
       <div className="card">
         <button>
           <a href="#">الذهاب للموقع</a>
